750c7d7155b641c6156ec1bc96775af1838bb7d8
teskew <teskinder1@gmail.com>
2020-11-19 14:03:51 -0600

    commit tree 65e73ab0f3e13eee31f830c8c67ab93181b8d6a0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+           "#{array[0]}"      
+    elsif array.length == 2
+        array[0..-1].join(" and ")
+    else
+        array[0...-1].join(", ") << ", and #{array[-1]}"
+    end
+    
+end 
